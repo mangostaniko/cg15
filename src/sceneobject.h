0da7de40c7ba65d9673cfeedb841d9c4ef4e4058
SEBA <seba.kirchner@gmail.com>
2015-04-10 21:12:38 +0200

    implements SceneObject header, uploads external libraries

--- src/sceneobject.h
+++ src/sceneobject.h
@@ -5,10 +5,21 @@
 
 class SceneObject
 {
+private:
+
+	glm::mat4 modelMatrix;
+
+	glm::mat4 inverseMatrix;
 
 public:
 	SceneObject(const glm::mat4 &modelMatrix_);
 	virtual ~SceneObject();
+
+	
+	/**
+	 * @brief enum that specifies the order of matrix multiplication
+	 */
+	enum Order {LEFT, RIGHT};
 
 	/**
 	 * @brief update the state of the SceneObject
@@ -21,8 +32,64 @@ public:
 	 */
 	virtual void draw() = 0;
 
-	glm::mat4 modelMatrix;
+	/**
+	 * @brief apply a transformation matrix to the current matrix
+	 * @param transform_ matrix defining a transformation
+	 * @param inverse_ the inverse matrix of transform
+	 * @param multOrder order of multiplication
+	 */
+	void applyTransformation(const glm::mat4 &transform_, const glm::mat4 &inverse_, Order multOrder);
 
+	/**
+	 * @return the const reference to the current model matrix
+	 */
+	const glm::mat4& getMatrix() const;
+
+	/**
+     * @brief replaces current matrix and sets its inverse
+	 * @param matrix_ the new transformation matrix
+	 */
+	void setTransform(const glm::mat4 &matrix_);
+
+	/**
+	* @return the constant reference to the inverse matrix of the current model matrix
+	*/
+	const glm::mat4& getInverseMatrix() const;
+
+	/**
+	* @brief applies an X axis rotation operation to the current transformation
+	* @param radians rotation angle
+	* @param multOrder order of multiplication
+	*/
+	void rotateX(float radians, Order multOrder);
+
+	/**
+	* @brief applies a Y axis rotation operation to the current transformation
+	* @param radians rotation angle
+	* @param multOrder order of multiplication
+	*/
+	void rotateY(float radians, Order multOrder);
+
+	/**
+	* @brief applies a Z axis rotation operation to the current transformation
+	* @param radians rotation angle
+	* @param multOrder order of multiplication
+	*/
+	void rotateZ(float radians, Order multOrder);
+
+	/**
+	* @brief applies a translation operation to the current transformation
+	* @param t translation vector
+	* @param multOrder order of multiplication
+	*/
+	void translate(const glm::vec3 &t_, Order multOrder);
+
+	/**
+	 * @brief applies a scale operation to the current transformation 
+	 * @param s scaling vector
+	 * @param multOrder order of multiplication
+	 */
+	void scale(const glm::vec3 &s_, Order multOrder);
 };
 
 #endif // SCENEOBJECT_H
