5ee08918a7242f3c8e28339adf5bc697c48b4b9f
Sebastian Kirchner <seba.kirchner@gmail.com>
2015-06-29 16:14:04 +0200

    small changes

--- SEGANKU/main.cpp
+++ SEGANKU/main.cpp
@@ -52,13 +52,6 @@ void drawText(double deltaT, int windowWidth, int windowHeight);
 void cleanup();
 void newGame();
 
-#ifdef _WIN32
-static void APIENTRY DebugCallbackAMD(GLuint id, GLenum category, GLenum severity, GLsizei length, const GLchar* message, GLvoid* userParam);
-static void APIENTRY DebugCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const GLvoid* userParam);
-std::string FormatDebugOutput(GLenum source, GLenum type, GLuint id, GLenum severity, const char* msg);
-void registerDebugCallbacks();
-#endif
-
 GLFWwindow *window;
 int windowWidth, windowHeight;
 bool running			       = true;
@@ -94,7 +87,7 @@ const glm::vec3 LIGHT_END(glm::vec3(20, 150, 0));
 std::vector<std::shared_ptr<Geometry>> carrots;
 std::vector<std::shared_ptr<Geometry>> trees;
 std::vector<std::shared_ptr<Geometry>> shrubs;
-const float timeToStarvation = 30;
+const float timeToStarvation = 120;
 
 // Shadow Map FBO and depth texture
 GLuint depthMapFBO, vsmDepthMapFBO;
@@ -102,7 +95,7 @@ GLuint depthMap, vsmDepthMap;
 GLuint pingpongFBO;
 GLuint pingpongColorMap;
 
-const int SM_WIDTH = 2048, SM_HEIGHT = 2048;
+const int SM_WIDTH = 4096, SM_HEIGHT = 4096;
 const GLfloat NEAR_PLANE = 75.f, FAR_PLANE = 250.f;
 
 void frameBufferResize(GLFWwindow *window, int width, int height);
@@ -176,11 +169,6 @@ int main(int argc, char **argv)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-#if _DEBUG
-    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
-	registerDebugCallbacks();
-#endif
 
 	GLFWmonitor *monitor = glfwGetPrimaryMonitor();
 	const GLFWvidmode *videoMode = glfwGetVideoMode(monitor);
@@ -458,8 +446,8 @@ void initPCFSM()
 	glBindTexture(GL_TEXTURE_2D, depthMap);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT24, SM_WIDTH, SM_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, 0);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
@@ -484,7 +472,7 @@ void initVSM()
 	// ShadowMomentsMap
 	glGenTextures(1, &vsmDepthMap);
 	glBindTexture(GL_TEXTURE_2D, vsmDepthMap);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, SM_WIDTH, SM_HEIGHT, 0, GL_RGBA, GL_FLOAT, 0);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB32F, SM_WIDTH, SM_HEIGHT, 0, GL_RGB, GL_FLOAT, 0);
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
@@ -1052,156 +1040,3 @@ void initWorldBounds(float &miX, float &maX, float &miZ, float &maZ)
 	miZ += 7; maZ -= 7;
 }
 
-
-#ifdef _WIN32
-
-#ifdef _DEBUG
-//////////////////////////
-/// DEBUG CONTEXT (WINDOWS)
-/// NOTE: add breakpoint in DebugCallback() function to find glerror origin from callstack
-//////////////////////////
-
-static void APIENTRY DebugCallbackAMD(GLuint id, GLenum category, GLenum severity, GLsizei length, const GLchar* message, GLvoid* userParam) {
-    std::string error = FormatDebugOutput(category, category, id, severity, message);
-    std::cout << error << std::endl; // ADD BREAKPOINT IN THIS LINE TO FIND GLERROR ORIGIN FROM CALLSTACK
-}
-
-static void APIENTRY DebugCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const GLvoid* userParam) {
-    std::string error = FormatDebugOutput(source, type, id, severity, message);
-    std::cout << error << std::endl; // ADD BREAKPOINT IN THIS LINE TO FIND GLERROR ORIGIN FROM CALLSTACK
-}
-
-std::string FormatDebugOutput(GLenum source, GLenum type, GLuint id, GLenum severity, const char* msg) {
-    std::stringstream stringStream;
-    std::string sourceString;
-    std::string typeString;
-    std::string severityString;
-
-    // The AMD variant of this extension provides a less detailed classification of the error,
-    // which is why some arguments might be "Unknown".
-    switch (source) {
-        case GL_DEBUG_CATEGORY_API_ERROR_AMD:
-        case GL_DEBUG_SOURCE_API: {
-            sourceString = "API";
-            break;
-        }
-        case GL_DEBUG_CATEGORY_APPLICATION_AMD:
-        case GL_DEBUG_SOURCE_APPLICATION: {
-            sourceString = "Application";
-            break;
-        }
-        case GL_DEBUG_CATEGORY_WINDOW_SYSTEM_AMD:
-        case GL_DEBUG_SOURCE_WINDOW_SYSTEM: {
-            sourceString = "Window System";
-            break;
-        }
-        case GL_DEBUG_CATEGORY_SHADER_COMPILER_AMD:
-        case GL_DEBUG_SOURCE_SHADER_COMPILER: {
-            sourceString = "Shader Compiler";
-            break;
-        }
-        case GL_DEBUG_SOURCE_THIRD_PARTY: {
-            sourceString = "Third Party";
-            break;
-        }
-        case GL_DEBUG_CATEGORY_OTHER_AMD:
-        case GL_DEBUG_SOURCE_OTHER: {
-            sourceString = "Other";
-            break;
-        }
-        default: {
-            sourceString = "Unknown";
-            break;
-        }
-    }
-
-    switch (type) {
-        case GL_DEBUG_TYPE_ERROR: {
-            typeString = "Error";
-            break;
-        }
-        case GL_DEBUG_CATEGORY_DEPRECATION_AMD:
-        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: {
-            typeString = "Deprecated Behavior";
-            break;
-        }
-        case GL_DEBUG_CATEGORY_UNDEFINED_BEHAVIOR_AMD:
-        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR: {
-            typeString = "Undefined Behavior";
-            break;
-        }
-        case GL_DEBUG_TYPE_PORTABILITY_ARB: {
-            typeString = "Portability";
-            break;
-        }
-        case GL_DEBUG_CATEGORY_PERFORMANCE_AMD:
-        case GL_DEBUG_TYPE_PERFORMANCE: {
-            typeString = "Performance";
-            break;
-        }
-        case GL_DEBUG_CATEGORY_OTHER_AMD:
-        case GL_DEBUG_TYPE_OTHER: {
-            typeString = "Other";
-            break;
-        }
-        default: {
-            typeString = "Unknown";
-            break;
-        }
-    }
-
-    switch (severity) {
-        case GL_DEBUG_SEVERITY_HIGH: {
-            severityString = "High";
-            break;
-        }
-        case GL_DEBUG_SEVERITY_MEDIUM: {
-            severityString = "Medium";
-            break;
-        }
-        case GL_DEBUG_SEVERITY_LOW: {
-            severityString = "Low";
-            break;
-        }
-        default: {
-            severityString = "Unknown";
-            break;
-        }
-    }
-
-    stringStream << "OpenGL Error: " << msg;
-    stringStream << " [Source = " << sourceString;
-    stringStream << ", Type = " << typeString;
-    stringStream << ", Severity = " << severityString;
-    stringStream << ", ID = " << id << "]";
-
-    return stringStream.str();
-}
-
-void registerDebugCallbacks()
-{
-    // Query the OpenGL function to register your callback function.
-    PFNGLDEBUGMESSAGECALLBACKPROC _glDebugMessageCallback = (PFNGLDEBUGMESSAGECALLBACKPROC) wglGetProcAddress("glDebugMessageCallback");
-    PFNGLDEBUGMESSAGECALLBACKARBPROC _glDebugMessageCallbackARB = (PFNGLDEBUGMESSAGECALLBACKARBPROC) wglGetProcAddress("glDebugMessageCallbackARB");
-    PFNGLDEBUGMESSAGECALLBACKAMDPROC _glDebugMessageCallbackAMD = (PFNGLDEBUGMESSAGECALLBACKAMDPROC) wglGetProcAddress("glDebugMessageCallbackAMD");
-
-    // Register your callback function.
-    if (_glDebugMessageCallback != NULL) {
-		_glDebugMessageCallback(DebugCallbackAMD, NULL);
-    }
-    else if (_glDebugMessageCallbackARB != NULL) {
-        _glDebugMessageCallbackARB(DebugCallback, NULL);
-    }
-    else if (_glDebugMessageCallbackAMD != NULL) {
-        _glDebugMessageCallbackAMD(DebugCallbackAMD, NULL);
-    }
-
-    // Enable synchronous callback. This ensures that your callback function is called
-    // right after an error has occurred. This capability is not defined in the AMD
-    // version.
-    if ((_glDebugMessageCallback != NULL) || (_glDebugMessageCallbackARB != NULL)) {
-        glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
-    }
-}
-#endif
-#endif
